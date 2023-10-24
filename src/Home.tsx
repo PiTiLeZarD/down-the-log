0c181e0434c9edd450633ec443211666a0c228dd
Jonathan Adami <pitilezard@gmail.com>
2023-10-24 14:27:12 +1000

    alright a step at a time ;)

---
+++ src/Home.tsx
@@ -0,0 +1,189 @@
+import { Text } from '@gluestack-ui/themed';
+
+import { Box } from '@gluestack-ui/themed';
+import React from 'react';
+
+const classes: Record<string, object> = {
+    container: {
+        display: 'flex',
+        width: '100%',
+    },
+    table: {
+        flexGrow: 1,
+    },
+};
+
+export type HomeProps = {};
+
+export type HomeComponent = React.FC<HomeProps>;
+
+export const Home: HomeComponent = (): JSX.Element => {
+    return (
+        <Box sx={classes.container}>
+            <Box sx={classes.top}>
+                <Text>Here the top part</Text>L
+            </Box>
+            <Box sx={classes.table}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Band</th>
+                            <th>CallSign</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>5</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>6</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>7</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>8</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>9</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>10</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>11</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>12</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>13</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>14</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>15</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>16</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>17</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>18</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>19</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>20</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>21</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>22</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>23</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>24</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>25</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>26</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>27</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>28</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                        <tr>
+                            <td>29</td>
+                            <td>20m</td>
+                            <td>VK4ALE</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </Box>
+            <Box sx={classes.inputs}>
+                <Text>Here the input</Text>
+            </Box>
+        </Box>
+    );
+};
