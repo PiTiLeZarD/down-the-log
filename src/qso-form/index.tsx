17bedb9bbfdba01ca5feecb3c8566eb738ca5549
Jonathan Adami <pitilezard@gmail.com>
2023-11-03 11:24:46 +1000

    clearly a better design for formfields

--- src/qso-form/index.tsx
+++ src/qso-form/index.tsx
@@ -1,37 +1,15 @@
-import {
-    ChevronDownIcon,
-    FormControl,
-    FormControlLabel,
-    HStack,
-    Icon,
-    Input,
-    InputField,
-    Select,
-    SelectBackdrop,
-    SelectContent,
-    SelectDragIndicator,
-    SelectDragIndicatorWrapper,
-    SelectIcon,
-    SelectInput,
-    SelectItem,
-    SelectPortal,
-    SelectTrigger,
-    Text,
-    Textarea,
-    TextareaInput,
-    VStack,
-} from '@gluestack-ui/themed';
+import { Box, Button, ButtonText, HStack, VStack } from '@gluestack-ui/themed';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { RootStackParamList } from '../RootStack';
 import { QSO, useStore } from '../store';
+import { FormField } from '../utils/form-field';
 
 const classes = {
     container: {
         padding: 10,
     },
-    hstack: {},
     hstackElt: {
         flex: 1,
     },
@@ -45,112 +23,48 @@ export const QsoForm: QsoFormComponent = ({ route }): JSX.Element => {
     const { qsoId } = route.params;
     const qso = useStore((state) => state.qsos).filter((q) => q.id == qsoId)[0];
 
-    const { register, watch, setValue } = useForm<QSO>({
+    const { handleSubmit, control } = useForm<QSO>({
         defaultValues: qso,
     });
 
-    const mode = watch('mode');
+    const onSubmit = (qso: QSO) => {
+        alert(JSON.stringify(qso));
+    };
 
     return (
         <VStack space="md" sx={classes.container}>
-            <HStack space="md" sx={classes.hstack}>
-                <FormControl sx={classes.hstackElt}>
-                    <FormControlLabel>
-                        <Text>Callsign:</Text>
-                    </FormControlLabel>
-                    <Input>
-                        <InputField {...register('callsign')} />
-                    </Input>
-                </FormControl>
+            <HStack space="md">
+                <Box sx={classes.hstackElt}>
+                    <FormField name="callsign" label="Callsign:" control={control} />
+                </Box>
 
-                <FormControl sx={classes.hstackElt}>
-                    <FormControlLabel>
-                        <Text>Name:</Text>
-                    </FormControlLabel>
-                    <Input>
-                        <InputField {...register('name')} />
-                    </Input>
-                </FormControl>
+                <Box sx={classes.hstackElt}>
+                    <FormField name="name" label="Name:" control={control} />
+                </Box>
             </HStack>
 
-            <HStack space="md" sx={classes.hstack}>
-                <FormControl sx={classes.hstackElt}>
-                    <FormControlLabel>
-                        <Text>Mode:</Text>
-                    </FormControlLabel>
-                    <Select
-                        {...register('mode')}
-                        selectedValue={mode}
-                        onValueChange={(mode) => setValue('mode', mode as QSO['mode'])}
-                    >
-                        <SelectTrigger variant="outline" size="md">
-                            <SelectInput placeholder="Select option" />
-                            <SelectIcon mr="$3">
-                                <Icon as={ChevronDownIcon} />
-                            </SelectIcon>
-                        </SelectTrigger>
-                        <SelectPortal>
-                            <SelectBackdrop />
-                            <SelectContent>
-                                <SelectDragIndicatorWrapper>
-                                    <SelectDragIndicator />
-                                </SelectDragIndicatorWrapper>
-                                <SelectItem label="SSB" value="SSB" />
-                                <SelectItem label="AM" value="AM" />
-                                <SelectItem label="FM" value="FM" />
-                                <SelectItem label="CW" value="CW" />
-                            </SelectContent>
-                        </SelectPortal>
-                    </Select>
-                </FormControl>
-
-                <FormControl sx={{ flex: 3 }}>
-                    <FormControlLabel>
-                        <Text>Frequency:</Text>
-                    </FormControlLabel>
-                    <Input>
-                        <InputField {...register('frequency')} />
-                    </Input>
-                </FormControl>
-
-                <FormControl sx={classes.hstackElt}>
-                    <FormControlLabel>
-                        <Text>Power:</Text>
-                    </FormControlLabel>
-                    <Input>
-                        <InputField {...register('power')} />
-                    </Input>
-                </FormControl>
+            <HStack space="md">
+                <FormField
+                    role="select"
+                    name="mode"
+                    label="Mode:"
+                    options={{ SSB: 'SSB', AM: 'AM', FM: 'FM', CW: 'CW' }}
+                    control={control}
+                />
+                <FormField name="frequency" label="Frequency:" control={control} />
+                <FormField name="power" label="Power:" control={control} />
             </HStack>
 
-            <HStack space="md" sx={classes.hstack}>
-                <FormControl sx={{ flex: 4 }}>
-                    <FormControlLabel>
-                        <Text>QTH:</Text>
-                    </FormControlLabel>
-                    <Input>
-                        <InputField {...register('qth')} />
-                    </Input>
-                </FormControl>
-
-                <FormControl sx={classes.hstackElt}>
-                    <FormControlLabel>
-                        <Text>Locator:</Text>
-                    </FormControlLabel>
-                    <Input>
-                        <InputField {...register('locator')} />
-                    </Input>
-                </FormControl>
+            <HStack space="md">
+                <FormField name="qth" label="QTH:" control={control} />
+                <FormField name="locator" label="Locator:" control={control} />
             </HStack>
 
-            <FormControl>
-                <FormControlLabel>
-                    <Text>Note:</Text>
-                </FormControlLabel>
-                <Textarea>
-                    <TextareaInput {...register('note')} />
-                </Textarea>
-            </FormControl>
+            <FormField role="textarea" name="note" label="Note:" control={control} />
+
+            <Button onPress={handleSubmit(onSubmit)}>
+                <ButtonText>Submit</ButtonText>
+            </Button>
         </VStack>
     );
 };
---
+++ src/utils/form-field.tsx
@@ -0,0 +1,78 @@
+import {
+    ChevronDownIcon,
+    FormControl,
+    FormControlLabel,
+    Icon,
+    Input,
+    InputField,
+    Select,
+    SelectBackdrop,
+    SelectContent,
+    SelectDragIndicator,
+    SelectDragIndicatorWrapper,
+    SelectIcon,
+    SelectInput,
+    SelectItem,
+    SelectPortal,
+    SelectTrigger,
+    Text,
+    Textarea,
+    TextareaInput,
+} from '@gluestack-ui/themed';
+import React from 'react';
+import { Control, useController } from 'react-hook-form';
+import { QSO } from '../store';
+
+export type FormFieldProps = {
+    name: keyof QSO;
+    label: string;
+    role?: 'text' | 'select' | 'textarea';
+    options?: Record<string, string>;
+    control: Control<QSO, string>;
+};
+
+export type FormFieldComponent = React.FC<FormFieldProps>;
+
+export const FormField: FormFieldComponent = ({ role = 'text', name, label, options, control }): JSX.Element => {
+    const { field } = useController({ name, control });
+
+    const value = String(field.value || '');
+    return (
+        <FormControl>
+            <FormControlLabel>
+                <Text>{label}</Text>
+            </FormControlLabel>
+            {role === 'text' && (
+                <Input>
+                    <InputField value={value} onChangeText={field.onChange} />
+                </Input>
+            )}
+            {role === 'select' && (
+                <Select selectedValue={value} onValueChange={field.onChange}>
+                    <SelectTrigger variant="outline" size="md">
+                        <SelectInput placeholder="Select option" />
+                        <SelectIcon mr="$3">
+                            <Icon as={ChevronDownIcon} />
+                        </SelectIcon>
+                    </SelectTrigger>
+                    <SelectPortal>
+                        <SelectBackdrop />
+                        <SelectContent>
+                            <SelectDragIndicatorWrapper>
+                                <SelectDragIndicator />
+                            </SelectDragIndicatorWrapper>
+                            {Object.entries(options || {}).map(([key, value]) => (
+                                <SelectItem label={value} value={key} key={key} />
+                            ))}
+                        </SelectContent>
+                    </SelectPortal>
+                </Select>
+            )}
+            {role === 'textarea' && (
+                <Textarea>
+                    <TextareaInput value={value} onChangeText={field.onChange} />
+                </Textarea>
+            )}
+        </FormControl>
+    );
+};
