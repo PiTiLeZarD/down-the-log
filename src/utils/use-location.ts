b43d29ca9549b04841b51db469642a5655889c48
Jonathan Adami <pitilezard@gmail.com>
2023-11-06 13:24:38 +1000

    bugfix here

--- src/utils/use-location.ts
+++ src/utils/use-location.ts
@@ -18,7 +18,7 @@ export const useLocation = () => {
             const location = await Location.getCurrentPositionAsync({});
             setLocation(location);
         })();
-    });
+    }, []);
 
     return location;
 };
