022967fa6afd6e343f4d414ab44ce8226dd76a30
Jonathan Adami <pitilezard@gmail.com>
2023-11-09 08:54:52 +1000

    parseCallsign is now using that database I've done

--- src/utils/callsign.ts
+++ src/utils/callsign.ts
@@ -1,29 +1,33 @@
+import { CallsignData, callsigns } from '../data/callsigns';
+
 const callsignRegexp = /^(([0-9]{0,1}[^0-9]+)([0-9]{1,2})\/)?([0-9]{0,1}[^0-9]+)([0-9]{1,2})([^\/]+)\/{0,1}(.*)$/g;
 
-const callsigns = [
-    [/F/, 'FRA'],
-    [/VK/, 'AUS'],
-    [/9W/, 'MYS'],
-    [/I[A-HKLNP]{0,1}/, 'ITA'],
-    [/D[A-DF-HJKP]/, 'DEU'],
-];
+type CsDataType = (CallsignData & { state?: string }) | undefined;
+const csdata = (prefix: string, index: string): CsDataType => {
+    const check = `${prefix}${index}`;
+    const data: CsDataType = callsigns.find((cd) => cd.regexp.test(check));
+    if (data?.states) {
+        data.state = Object.entries(data.states).reduce<string | undefined>(
+            (acc, [state, regexp]) => acc || (regexp.test(check) ? state : undefined),
+            undefined
+        );
+    }
+    return data;
+};
 
 export const parseCallsign = (callsign: string) => {
     const [, , locPrefix, locIndex, prefix, index, delineation, suffix] = Array.from(
         callsign.matchAll(callsignRegexp)
     )[0];
 
-    const country = callsigns.reduce<string | undefined>(
-        (acc, [regexp, iso3]) => acc || (prefix.match(regexp as RegExp) ? (iso3 as string) : undefined),
-        undefined
-    );
-
-    const locCountry = locPrefix
-        ? callsigns.reduce<string | undefined>(
-              (acc, [regexp, iso3]) => acc || (locPrefix.match(regexp as RegExp) ? (iso3 as string) : undefined),
-              undefined
-          )
-        : undefined;
-
-    return { locPrefix, locIndex, prefix, index, delineation, suffix, country, locCountry };
+    return {
+        locPrefix,
+        locIndex,
+        prefix,
+        index,
+        delineation,
+        suffix,
+        data: csdata(prefix, index),
+        ...(locPrefix ? { locData: csdata(locPrefix, locIndex) } : {}),
+    };
 };
