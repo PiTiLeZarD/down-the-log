7f7de2bbf12e9d4405f7177e3986cb32d1cc9c2f
Jonathan Adami <pitilezard@gmail.com>
2023-11-12 21:21:52 +1000

    add a header and parse it too, starting to read adif spec ;)

--- src/utils/adif.ts
+++ src/utils/adif.ts
@@ -1,29 +1,43 @@
-import { DateTime } from 'luxon';
-import { freq2band } from '../data/bands';
-import cqzones from '../data/cqzones.json';
-import ituzones from '../data/ituzones.json';
-import { getCallsignData } from './callsign';
-import { maidenhead2Latlong } from './locator';
-import { findZone } from './polydec';
-import { QSO } from './qso';
+import { DateTime } from "luxon";
+import { freq2band } from "../data/bands";
+import cqzones from "../data/cqzones.json";
+import ituzones from "../data/ituzones.json";
+import { getCallsignData } from "./callsign";
+import { maidenhead2Latlong } from "./locator";
+import { findZone } from "./polydec";
+import { QSO } from "./qso";
+
+const header = (): string[] => [
+    "ADIF Export from down-the-log by VK4ALE",
+    "for further info visit: https://github.com/PiTiLeZarD/down-the-log",
+    "",
+    field("adif_ver", "3.1.4"),
+    field("created_timestamp", DateTime.utc().toFormat("yyyyMMdd HHmmss")),
+    field("programid", "down-the-log"),
+    field("programversion", "0.0.1"),
+    "<EOH>",
+    "",
+    "",
+];
 
 const field = (label: string, value?: string): string =>
-    typeof value !== 'undefined' && value !== null
-        ? '<' + label.toUpperCase() + ':' + ('' + value).length + '>' + value
-        : '';
+    typeof value !== "undefined" && value !== null
+        ? "<" + label.toUpperCase() + ":" + ("" + value).length + ">" + value
+        : "";
 
 const parseField = (adif: string): string[] => {
     let remaining = adif.trim();
-    const match = remaining.match(/[<]([^:]+)[:]([0-9]+)[>]/);
+    const match = remaining.match(/[<]([^:]+)[:]([0-9]+)([:]([^<]+))?[>]/);
     if (!match) {
-        console.error('Error while parsing line');
+        console.error("Error while parsing line");
         console.error(remaining);
         return [];
     }
-    const [, tagName, tagLength] = match;
-    remaining = remaining.slice(`<${tagName}:${tagLength}>`.length);
+    const [, tagName, tagLength, , tagType] = match;
+
+    remaining = remaining.slice(`<${tagName}:${tagLength}${tagType ? `:${tagType}` : ""}>`.length);
     const value = remaining.slice(0, +tagLength);
-    remaining = remaining.slice(+tagLength);
+    remaining = remaining.slice(+tagLength).trim();
     return [remaining, tagName.toLowerCase(), value];
 };
 
@@ -31,42 +45,42 @@ export const qso2adif = (qso: QSO): string => {
     const callsignData = getCallsignData(qso.callsign);
     return (
         [
-            qso.date ? field('qso_date', qso.date.toFormat('yyyyMMdd')) : null,
-            qso.date ? field('time_on', qso.date.toFormat('HHmmss')) : null,
+            qso.date ? field("qso_date", qso.date.toFormat("yyyyMMdd")) : null,
+            qso.date ? field("time_on", qso.date.toFormat("HHmmss")) : null,
 
-            field('band', freq2band(qso.frequency) as string),
-            qso.frequency ? field('freq', String(qso.frequency)) : null,
-            field('mode', qso.mode),
-            qso.power ? field('tx_pwr', String(qso.power)) : null,
+            field("band", freq2band(qso.frequency) as string),
+            qso.frequency ? field("freq", String(qso.frequency)) : null,
+            field("mode", qso.mode),
+            qso.power ? field("tx_pwr", String(qso.power)) : null,
 
-            field('call', qso.callsign),
-            field('state', callsignData?.state),
-            field('name', qso.name),
+            field("call", qso.callsign),
+            field("state", callsignData?.state),
+            field("name", qso.name),
 
-            field('qth', qso.qth),
-            field('gridsquare', qso.locator),
-            field('cont', callsignData?.ctn),
-            qso.locator ? field('cqz', findZone(cqzones, maidenhead2Latlong(qso.locator))) : null,
-            qso.locator ? field('ituz', findZone(ituzones, maidenhead2Latlong(qso.locator))) : null,
+            field("qth", qso.qth),
+            field("gridsquare", qso.locator),
+            field("cont", callsignData?.ctn),
+            qso.locator ? field("cqz", findZone(cqzones, maidenhead2Latlong(qso.locator))) : null,
+            qso.locator ? field("ituz", findZone(ituzones, maidenhead2Latlong(qso.locator))) : null,
         ]
             .filter((v) => !!v)
-            .join(' ') + '<EOR>'
+            .join(" ") + "<EOR>"
     );
 };
 
-export const qsos2Adif = (qsos: QSO[]): string => qsos.map(qso2adif).join('\n');
+export const qsos2Adif = (qsos: QSO[]): string => [...header(), ...qsos.map(qso2adif)].join("\n");
 
 export const adifLine2Qso = (adif: string): QSO | null => {
-    if (!adif.endsWith('<EOR>')) return null;
+    if (!adif.toUpperCase().endsWith("<EOR>")) return null;
 
     const qsoData: Record<string, string> = {};
     let tagName: string, value: string;
     let remaining = adif.trim();
 
-    while (remaining != '<EOR>' && remaining.length) {
+    while (remaining.length && remaining.toUpperCase() !== "<EOR>") {
         const field = parseField(remaining);
         if (field.length === 0) {
-            remaining = '';
+            remaining = "";
             continue;
         }
 
@@ -74,7 +88,7 @@ export const adifLine2Qso = (adif: string): QSO | null => {
         qsoData[tagName] = value;
     }
 
-    const date = DateTime.fromFormat(`${qsoData.qso_date} ${qsoData.time_on}`, 'yyyyMMdd HHmmss');
+    const date = DateTime.fromFormat(`${qsoData.qso_date} ${qsoData.time_on}`, "yyyyMMdd HHmmss");
 
     return {
         date,
@@ -90,14 +104,16 @@ export const adifLine2Qso = (adif: string): QSO | null => {
     } as any as QSO;
 };
 
-export const adifFile2Qso = (adif: string): QSO[] =>
-    adif
-        .split('\n')
-        .map(adifLine2Qso)
-        .filter((q) => !!q) as QSO[];
+export const adifFile2Qso = (adif: string): QSO[] => {
+    let lines = adif.split("\n");
+    if (!lines[0].startsWith("<")) {
+        lines = lines.splice(lines.findIndex((v) => v.toUpperCase().startsWith("<EOH>")) + 1);
+    }
+    return lines.map((l) => adifLine2Qso(l.trim())).filter((q) => !!q) as QSO[];
+};
 
 export const downloadQsos = (title: string, qsos: QSO[]) =>
-    Object.assign(document.createElement('a'), {
+    Object.assign(document.createElement("a"), {
         href: `data:text/plain, ${encodeURIComponent(qsos2Adif(qsos))}`,
         download: title,
     }).click();
