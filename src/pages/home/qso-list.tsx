31a0617f619d0e6135147380700c3be07ead25cb
Jonathan Adami <pitilezard@gmail.com>
2023-12-16 12:42:53 +1000

    This is somewhat improving speed and readability, but there's still something weird

--- src/pages/home/qso-list.tsx
+++ src/pages/home/qso-list.tsx
@@ -9,6 +9,58 @@ import { Stack } from "../../utils/stack";
 import { Typography } from "../../utils/theme/components/typography";
 import { Qso } from "./qso";
 
+export type QsoListItemProps = {
+    qsos: QSO[];
+    item: QSO;
+    index: number;
+    onQsoPress: QsoListProps["onQsoPress"];
+};
+
+export type QsoListItemComponent = React.FC<QsoListItemProps>;
+
+export const QsoListItem: QsoListItemComponent = React.memo(
+    ({ qsos, item: qso, index, onQsoPress }): JSX.Element => {
+        const currentLocation = useStore((state) => state.currentLocation);
+        const callsignData = getCallsignData(qso.callsign);
+        return (
+            <>
+                {(index === 0 || qso.date.diff(qsos[index - 1].date).days > 0) && (
+                    <>
+                        <View
+                            style={{
+                                paddingHorizontal: 5,
+                                paddingVertical: 3,
+                                display: "flex",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Typography style={{ fontWeight: "bold" }}>{qso.date.toFormat("dd/MM/yyyy")}</Typography>
+                        </View>
+                    </>
+                )}
+                <Qso
+                    position={String(index + 1)}
+                    time={qso.date.toFormat("HH:mm")}
+                    callsign={
+                        <Stack direction="row">
+                            <Typography>{findCountry(callsignData)?.flag}</Typography>
+                            <Typography>{qso.callsign}</Typography>
+                            <Typography>
+                                ({maidenDistance(currentLocation, qso.locator || callsignData?.gs || currentLocation)}
+                                km)
+                            </Typography>
+                        </Stack>
+                    }
+                    name={qso.name || "N/A"}
+                    band={`${qso.frequency ? freq2band(+qso.frequency) || "N/A" : "N/A"} (${qso.mode || "N/A"})`}
+                    onPress={() => onQsoPress(qso)}
+                />
+            </>
+        );
+    },
+    (prevProps, nextProps) => nextProps.item.id === prevProps.item.id,
+);
+
 export type QsoListProps = {
     qsos: QSO[];
     onQsoPress: (qso: QSO) => void;
@@ -16,56 +68,10 @@ export type QsoListProps = {
 
 export type QsoListComponent = React.FC<QsoListProps>;
 
-export const QsoList: QsoListComponent = ({ qsos, onQsoPress }): JSX.Element => {
-    const currentLocation = useStore((state) => state.currentLocation);
-    const separator = (qso: QSO, index: number): JSX.Element => {
-        if (index === 0 || qso.date.diff(qsos[index - 1].date).days > 0) {
-            return (
-                <View
-                    style={{
-                        paddingHorizontal: 5,
-                        paddingVertical: 3,
-                        display: "flex",
-                        alignItems: "center",
-                    }}
-                >
-                    <Typography style={{ fontWeight: "bold" }}>{qso.date.toFormat("dd/MM/yyyy")}</Typography>
-                </View>
-            );
-        }
-        return <></>;
-    };
-
-    const callsignCell = (qso: QSO) => {
-        const callsignData = getCallsignData(qso.callsign);
-        return (
-            <Stack direction="row">
-                <Typography>{findCountry(callsignData)?.flag}</Typography>
-                <Typography>{qso.callsign}</Typography>
-                <Typography>
-                    ({maidenDistance(currentLocation, qso.locator || callsignData?.gs || currentLocation)}km)
-                </Typography>
-            </Stack>
-        );
-    };
-
-    return (
-        <FlatList
-            ListHeaderComponent={<Qso header position="ID" time="Time" callsign="Callsign" name="Name" band="Band" />}
-            data={qsos}
-            renderItem={({ item, index }) => (
-                <>
-                    {separator(item, index)}
-                    <Qso
-                        position={String(index + 1)}
-                        time={item.date.toFormat("HH:mm")}
-                        callsign={callsignCell(item)}
-                        name={item.name || "N/A"}
-                        band={`${item.frequency ? freq2band(+item.frequency) || "N/A" : "N/A"} (${item.mode || "N/A"})`}
-                        onPress={() => onQsoPress(item)}
-                    />
-                </>
-            )}
-        />
-    );
-};
+export const QsoList: QsoListComponent = ({ qsos, onQsoPress }): JSX.Element => (
+    <FlatList
+        ListHeaderComponent={<Qso header position="ID" time="Time" callsign="Callsign" name="Name" band="Band" />}
+        data={qsos}
+        renderItem={({ item, index }) => <QsoListItem {...{ qsos, onQsoPress, item, index }} />}
+    />
+);
