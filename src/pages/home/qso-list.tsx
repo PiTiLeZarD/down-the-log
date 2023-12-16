c4d2fe10b6d68ed5421d8eadd0ad839a1a714978
Jonathan Adami <pitilezard@gmail.com>
2023-12-16 13:24:14 +1000

    Better use of the lists but considerably slower

--- src/pages/home/qso-list.tsx
+++ src/pages/home/qso-list.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, View } from "react-native";
+import { SectionList, View } from "react-native";
 import { freq2band } from "../../data/bands";
 import { useStore } from "../../store";
 import { findCountry, getCallsignData } from "../../utils/callsign";
@@ -9,8 +9,21 @@ import { Stack } from "../../utils/stack";
 import { Typography } from "../../utils/theme/components/typography";
 import { Qso } from "./qso";
 
+type QSOSection = {
+    title: string;
+    data: QSO[];
+};
+
+const qsos2sections = (qsos: QSO[]): QSOSection[] =>
+    Object.entries(
+        qsos.reduce<Record<string, QSO[]>>((sections, qso) => {
+            const title = qso.date.toFormat("dd/MM/yyyy");
+            sections[title] = [...(sections[title] || []), qso];
+            return sections;
+        }, {}),
+    ).map(([title, data]) => ({ title, data }) as QSOSection);
+
 export type QsoListItemProps = {
-    qsos: QSO[];
     item: QSO;
     index: number;
     onQsoPress: QsoListProps["onQsoPress"];
@@ -19,43 +32,27 @@ export type QsoListItemProps = {
 export type QsoListItemComponent = React.FC<QsoListItemProps>;
 
 export const QsoListItem: QsoListItemComponent = React.memo(
-    ({ qsos, item: qso, index, onQsoPress }): JSX.Element => {
+    ({ item: qso, index, onQsoPress }): JSX.Element => {
         const currentLocation = useStore((state) => state.currentLocation);
         const callsignData = getCallsignData(qso.callsign);
         return (
-            <>
-                {(index === 0 || qso.date.diff(qsos[index - 1].date).days > 0) && (
-                    <>
-                        <View
-                            style={{
-                                paddingHorizontal: 5,
-                                paddingVertical: 3,
-                                display: "flex",
-                                alignItems: "center",
-                            }}
-                        >
-                            <Typography style={{ fontWeight: "bold" }}>{qso.date.toFormat("dd/MM/yyyy")}</Typography>
-                        </View>
-                    </>
-                )}
-                <Qso
-                    position={String(index + 1)}
-                    time={qso.date.toFormat("HH:mm")}
-                    callsign={
-                        <Stack direction="row">
-                            <Typography>{findCountry(callsignData)?.flag}</Typography>
-                            <Typography>{qso.callsign}</Typography>
-                            <Typography>
-                                ({maidenDistance(currentLocation, qso.locator || callsignData?.gs || currentLocation)}
-                                km)
-                            </Typography>
-                        </Stack>
-                    }
-                    name={qso.name || "N/A"}
-                    band={`${qso.frequency ? freq2band(+qso.frequency) || "N/A" : "N/A"} (${qso.mode || "N/A"})`}
-                    onPress={() => onQsoPress(qso)}
-                />
-            </>
+            <Qso
+                position={String(index + 1)}
+                time={qso.date.toFormat("HH:mm")}
+                callsign={
+                    <Stack direction="row">
+                        <Typography>{findCountry(callsignData)?.flag}</Typography>
+                        <Typography>{qso.callsign}</Typography>
+                        <Typography>
+                            ({maidenDistance(currentLocation, qso.locator || callsignData?.gs || currentLocation)}
+                            km)
+                        </Typography>
+                    </Stack>
+                }
+                name={qso.name || "N/A"}
+                band={`${qso.frequency ? freq2band(+qso.frequency) || "N/A" : "N/A"} (${qso.mode || "N/A"})`}
+                onPress={() => onQsoPress(qso)}
+            />
         );
     },
     (prevProps, nextProps) => nextProps.item.id === prevProps.item.id,
@@ -69,9 +66,27 @@ export type QsoListProps = {
 export type QsoListComponent = React.FC<QsoListProps>;
 
 export const QsoList: QsoListComponent = ({ qsos, onQsoPress }): JSX.Element => (
-    <FlatList
+    <SectionList
         ListHeaderComponent={<Qso header position="ID" time="Time" callsign="Callsign" name="Name" band="Band" />}
-        data={qsos}
-        renderItem={({ item, index }) => <QsoListItem {...{ qsos, onQsoPress, item, index }} />}
+        sections={qsos2sections(qsos)}
+        keyExtractor={(item) => item.id}
+        initialNumToRender={25}
+        renderItem={({ item, index }) => <QsoListItem {...{ onQsoPress, item, index }} />}
+        renderSectionHeader={({ section }) => (
+            <View
+                style={{
+                    paddingHorizontal: 5,
+                    paddingVertical: 3,
+                    display: "flex",
+                    alignItems: "center",
+                    borderStyle: "solid",
+                    borderColor: "black",
+                    borderTopWidth: 1,
+                    borderBottomWidth: 1,
+                }}
+            >
+                <Typography style={{ fontWeight: "bold" }}>{section.title}</Typography>
+            </View>
+        )}
     />
 );
