4c0cb6e04b1c3e3d9ea120f439849b37b7fc28fa
Jonathan Adami <pitilezard@gmail.com>
2023-12-18 13:22:34 +1000

    fix expo 49's inability to load react-hook-form

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,8 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+// config.transformer.minifierConfig.compress.drop_console = true;
+config.resolver.sourceExts = [...config.resolver.sourceExts, "mjs", "cjs"];
+
+module.exports = config;
