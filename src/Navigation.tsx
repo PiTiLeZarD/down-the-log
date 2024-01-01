4326bd33172c3c2b1cbe63e4f863c3b08e145272
Jonathan Adami <pitilezard@gmail.com>
2024-01-01 12:59:39 +1000

    I feel like I'm getting somehwere but I'm defo not there yet

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -4,7 +4,8 @@ import {
     DrawerItem,
     createDrawerNavigator,
 } from "@react-navigation/drawer";
-import { View } from "react-native";
+import { useRef } from "react";
+import { ScrollView, View } from "react-native";
 import { QSO } from "./utils/qso";
 
 export type NavigationParamList = {
@@ -18,8 +19,37 @@ export const Navigation = createDrawerNavigator<NavigationParamList>();
 export type DrawerContentComponent = React.FC<DrawerContentComponentProps>;
 
 export const DrawerContent: DrawerContentComponent = (props): JSX.Element => {
+    const drawerRef = useRef<ScrollView>(null);
+
+    // const { width } = useWindowDimensions();
+
+    // const handleUpdate = () => {
+    //     if (drawerRef.current) {
+    //         const parent = (drawerRef.current as unknown as Node).parentNode;
+    //         const open = +(parent?.parentNode?.firstChild?.lastChild as HTMLElement)?.style.opacity === 1;
+    //         const styles = (parent as HTMLElement)?.style;
+
+    //         const drawerWidth = +styles.width.replace("px", "");
+    //         const offset = +styles.transform.substring(11, styles.transform.length - 3);
+
+    //         ((drawerRef.current as unknown as Node)?.parentNode as HTMLElement).style!.transform = `translateX(${
+    //             open ? width - drawerWidth : width
+    //         }px)`;
+    //     }
+    // };
+    // const observer = new MutationObserver((mutations) => mutations.forEach(handleUpdate));
+
+    // useEffect(() => {
+    //     if (drawerRef.current) {
+    //         const parent = (drawerRef.current as unknown as Node).parentNode;
+    //         observer.observe(parent as Node, { attributes: true, attributeFilter: ["style"] });
+    //     }
+    //     return () => observer.disconnect();
+    // }, [drawerRef]);
+    // useEffect(() => handleUpdate, [drawerRef, width]);
+
     return (
-        <DrawerContentScrollView {...props}>
+        <DrawerContentScrollView ref={drawerRef} {...props}>
             <View>
                 <DrawerItem label="Home" onPress={() => props.navigation.navigate("Home")} />
                 <DrawerItem label="Import/Export" onPress={() => props.navigation.navigate("Adif")} />
