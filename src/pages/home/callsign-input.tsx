b3fe1ab8ed903b3e9b894a640dcd8ab81df5181a
Jonathan Adami <pitilezard@gmail.com>
2024-01-13 12:56:25 +1000

    Use the space a little better here

--- src/pages/home/callsign-input.tsx
+++ src/pages/home/callsign-input.tsx
@@ -74,66 +74,61 @@ export const CallsignInput: CallsignInputComponent = ({ value, handleAdd, onChan
         }, 500),
         [value],
     );
-    console.log({ callsignData, hamqthCSData });
-
     const country = callsignData ? findCountry(callsignData) : null;
 
     return (
         <Stack style={styles.inputBox}>
             {callsignData && (
                 <Grid container>
-                    <Grid item xs={4}>
-                        <Typography>
-                            {country?.flag}
-                            {country?.name} {callsignData.state ? `(${callsignData.state})` : ""}
-                        </Typography>
+                    <Grid item xs={8} md={6} lg={8}>
+                        <Stack>
+                            {hamqthCSData && <Typography>{hamqthCSData.qth}</Typography>}
+                            <Stack direction="row">
+                                <Typography>{country?.flag}</Typography>
+                                <Typography>{country?.name}</Typography>
+                                {callsignData.state && <Typography>{callsignData.state}</Typography>}
+                                <Typography>({callsignData.ctn})</Typography>
+                            </Stack>
+                            <Typography variant="subtitle">
+                                {maidenDistance(
+                                    currentLocation,
+                                    hamqthCSData && hamqthCSData.grid ? hamqthCSData.grid : callsignData.gs,
+                                    settings.imperial,
+                                )}
+                                {settings.imperial ? "mi" : "km"}
+                            </Typography>
+                        </Stack>
                     </Grid>
-                    <Grid item xs={3}>
-                        <Typography>
-                            {maidenDistance(
-                                currentLocation,
-                                hamqthCSData && hamqthCSData.grid ? hamqthCSData.grid : callsignData.gs,
-                                settings.imperial,
-                            )}
-                            {settings.imperial ? "mi" : "km"}
-                        </Typography>
+                    <Grid item xs={4} md={3} lg={3}>
+                        {hamqthCSData && (
+                            <Stack>
+                                <Typography>{hamqthCSData.name}</Typography>
+                                <Typography variant="subtitle">
+                                    Local time:{" "}
+                                    {DateTime.utc()
+                                        .setZone(
+                                            `UTC${
+                                                hamqthCSData.utc_offset > 0
+                                                    ? `+${hamqthCSData.utc_offset}`
+                                                    : hamqthCSData.utc_offset
+                                            }`,
+                                        )
+                                        .toFormat("HH:mm")}
+                                </Typography>
+                                {hamqthCSData.age && <Typography variant="subtitle">{hamqthCSData.age}yo</Typography>}
+                            </Stack>
+                        )}
                     </Grid>
-                    <Grid item xs={5} md={3}>
-                        <Typography>
-                            CQ: {callsignData.gs ? findZone(cqzones, maidenhead2Latlong(callsignData.gs)) : "??"}, ITU:{" "}
-                            {callsignData.gs ? findZone(ituzones, maidenhead2Latlong(callsignData.gs)) : "??"}, DXCC:{" "}
-                            {callsignData.dxcc}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={-1} md={1}>
-                        <Typography>({callsignData.ctn})</Typography>
-                    </Grid>
-                </Grid>
-            )}
-            {hamqthCSData && (
-                <Grid container>
-                    <Grid item xs={3}>
-                        <Typography>{hamqthCSData.name}</Typography>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Typography>QTH: {hamqthCSData.qth}</Typography>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Typography>{hamqthCSData.age}yo</Typography>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Typography>
-                            Local time:{" "}
-                            {DateTime.utc()
-                                .setZone(
-                                    `UTC${
-                                        hamqthCSData.utc_offset > 0
-                                            ? `+${hamqthCSData.utc_offset}`
-                                            : hamqthCSData.utc_offset
-                                    }`,
-                                )
-                                .toFormat("HH:mm")}
-                        </Typography>
+                    <Grid item xs={-1} md={3} lg={1}>
+                        <Stack>
+                            <Typography variant="subtitle">
+                                CQ: {callsignData.gs ? findZone(cqzones, maidenhead2Latlong(callsignData.gs)) : "??"}
+                            </Typography>
+                            <Typography variant="subtitle">
+                                ITU: {callsignData.gs ? findZone(ituzones, maidenhead2Latlong(callsignData.gs)) : "??"}
+                            </Typography>
+                            <Typography variant="subtitle">DXCC: {callsignData.dxcc}</Typography>
+                        </Stack>
                     </Grid>
                 </Grid>
             )}
