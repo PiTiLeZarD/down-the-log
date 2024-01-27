8a7d12dc1cebf66a1fd88bb66010f1e713ef8dac
Jonathan Adami <pitilezard@gmail.com>
2024-01-27 22:53:10 +1000

    allow changing the text on gmaps chip

--- src/pages/qso-form/gmaps-chip.tsx
+++ src/pages/qso-form/gmaps-chip.tsx
@@ -3,16 +3,17 @@ import { maidenhead2Latlong } from "../../utils/locator";
 import { Button } from "../../utils/theme/components/button";
 
 export type GmapsChipProps = {
+    text?: string;
     locator?: string;
     zoom?: number;
 };
 
 export type GmapsChipComponent = React.FC<GmapsChipProps>;
 
-export const GmapsChip: GmapsChipComponent = ({ locator, zoom = 4 }): JSX.Element => {
+export const GmapsChip: GmapsChipComponent = ({ text = "gmaps", locator, zoom = 4 }): JSX.Element => {
     const latlng = locator ? maidenhead2Latlong(locator) : undefined;
     const url = latlng
         ? `https://www.google.com/maps/place/${latlng.latitude}+${latlng.longitude}/@${latlng.latitude},${latlng.longitude},${zoom}z?entry=ttu`
         : undefined;
-    return <Button variant="chip" text="gmaps" colour={latlng ? "primary" : "grey"} url={url} />;
+    return <Button variant="chip" text={text} colour={latlng ? "primary" : "grey"} url={url} />;
 };
