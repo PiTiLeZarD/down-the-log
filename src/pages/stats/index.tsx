6f8191df66cfce60a884ad4a986a4113889b318f
Jonathan Adami <pitilezard@gmail.com>
2024-01-27 20:17:50 +1000

    you can now control filters axis independently and also apply favourites or not

--- src/pages/stats/index.tsx
+++ src/pages/stats/index.tsx
@@ -1,9 +1,9 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import React from "react";
+import { Switch } from "react-native";
 import { NavigationParamList } from "../../Navigation";
-import { Band, bands, freq2band } from "../../data/bands";
-import { useStore } from "../../store";
-import { groupBy, sortNumsAndAlpha } from "../../utils/arrays";
+import { Settings, useStore } from "../../store";
+import { groupBy, sortNumsAndAlpha, unique } from "../../utils/arrays";
 import { Grid } from "../../utils/grid";
 import { PageLayout } from "../../utils/page-layout";
 import { QSO, useQsos } from "../../utils/qso";
@@ -12,67 +12,96 @@ import { SelectInput } from "../../utils/theme/components/select-input";
 import { Typography } from "../../utils/theme/components/typography";
 import { FilterName, Filters, filterMap, filterQsos } from "../home/filters";
 
-const groupQsos = (qsos: QSO[], statType: FilterName): Record<string, Record<string, QSO[]>> =>
+const groupQsos = (
+    qsos: QSO[],
+    first: FilterName,
+    second: FilterName = "band",
+): Record<string, Record<string, QSO[]>> =>
     Object.fromEntries(
-        Object.entries(groupBy(qsos, filterMap[statType])).map(([k, qs]) => [
-            k,
-            groupBy<QSO, string>(
-                qs.filter((q) => !!q.frequency),
-                (q) => freq2band(q.frequency) as Band,
-            ),
-        ]),
+        Object.entries(groupBy(qsos, filterMap[first])).map(([k, qs]) => [k, groupBy(qs, filterMap[second])]),
     );
+
+const applyFavourites = (values: string[], stat: FilterName, settings: Settings, useFavourites: boolean) => {
+    if (!useFavourites || !["band", "mode"].includes(stat)) return values;
+
+    const favourites = {
+        band: settings.favouriteBands,
+        mode: settings.favouriteModes,
+    }[stat] as string[];
+
+    return values.filter((v) => favourites.includes(v));
+};
 
 export type StatsProps = {} & StackScreenProps<NavigationParamList, "Stats">;
 
 export type StatsComponent = React.FC<StatsProps>;
 
 export const Stats: StatsComponent = ({ navigation }): JSX.Element => {
-    const [statType, setStatType] = React.useState<FilterName>("mode");
+    const [firstStat, setFirstStat] = React.useState<FilterName>("mode");
+    const [secondStat, setSecondStat] = React.useState<FilterName>("band");
+    const [useFavourites, setUseFavourites] = React.useState<boolean>(true);
     const qsosFilters = useStore((state) => state.filters);
     const qsos = filterQsos(useQsos(), qsosFilters);
-    const groups = groupQsos(qsos, statType);
-    const usedBands = [
-        ...new Set(
-            Object.values(groups)
-                .map((group) => Object.keys(group))
-                .flat(),
-        ),
-    ].sort((b1, b2) => Object.keys(bands).indexOf(b1) - Object.keys(bands).indexOf(b2));
-    const columns = usedBands.length + 2;
+    const groups = groupQsos(qsos, firstStat, secondStat);
+    const settings = useStore((state) => state.settings);
+    // const usedBands = [
+    //     ...new Set(
+    //         Object.values(groups)
+    //             .map((group) => Object.keys(group))
+    //             .flat(),
+    //     ),
+    // ].sort((b1, b2) => Object.keys(bands).indexOf(b1) - Object.keys(bands).indexOf(b2));
+    const secondStatValues = applyFavourites(
+        unique(qsos.map((q) => filterMap[secondStat](q))),
+        secondStat,
+        settings,
+        useFavourites,
+    );
+    const columns = secondStatValues.length + 2;
 
     return (
         <PageLayout title="Stats" navigate={navigation.navigate}>
             <Stack>
                 <Filters />
-                <SelectInput
-                    value={statType}
-                    onValueChange={(newStatType) => setStatType(newStatType)}
-                    items={Object.keys(filterMap).map((t) => ({ label: t, value: t }))}
-                />
+                <Stack direction="row">
+                    <Typography>Left side:</Typography>
+                    <SelectInput
+                        value={firstStat}
+                        onValueChange={(newStatType) => setFirstStat(newStatType)}
+                        items={Object.keys(filterMap).map((t) => ({ label: t, value: t }))}
+                    />
+                    <Typography>Top side:</Typography>
+                    <SelectInput
+                        value={secondStat}
+                        onValueChange={(newStatType) => setSecondStat(newStatType)}
+                        items={Object.keys(filterMap).map((t) => ({ label: t, value: t }))}
+                    />
+                    <Typography>Use favourites when available</Typography>
+                    <Switch value={useFavourites} onValueChange={(v) => setUseFavourites(v)} />
+                </Stack>
                 <Grid container>
                     <Grid item columns={columns} xs={1}>
-                        <Typography variant="em">{statType}</Typography>
+                        <Typography variant="em">{firstStat}</Typography>
                     </Grid>
-                    {usedBands.map((band) => (
-                        <Grid item columns={columns} xs={1} key={band}>
-                            <Typography variant="em">{band}</Typography>
+                    {secondStatValues.map((v) => (
+                        <Grid item columns={columns} xs={1} key={v}>
+                            <Typography variant="em">{v}</Typography>
                         </Grid>
                     ))}
                     <Grid item columns={columns} xs={1}>
                         <Typography variant="em">Total</Typography>
                     </Grid>
                 </Grid>
-                {Object.keys(groups)
+                {applyFavourites(Object.keys(groups), firstStat, settings, useFavourites)
                     .sort(sortNumsAndAlpha)
                     .map((group) => (
                         <Grid container key={group}>
                             <Grid item columns={columns} xs={1}>
                                 <Typography variant="em">{group}</Typography>
                             </Grid>
-                            {usedBands.map((band) => (
-                                <Grid item columns={columns} xs={1} key={`${group}_${band}`}>
-                                    <Typography>{(groups[group][band] || []).length}</Typography>
+                            {secondStatValues.map((v) => (
+                                <Grid item columns={columns} xs={1} key={`${group}_${v}`}>
+                                    <Typography>{(groups[group][v] || []).length}</Typography>
                                 </Grid>
                             ))}
                             <Grid item columns={columns} xs={1}>
@@ -84,11 +113,11 @@ export const Stats: StatsComponent = ({ navigation }): JSX.Element => {
                     <Grid item columns={columns} xs={1}>
                         <Typography variant="em">Total</Typography>
                     </Grid>
-                    {usedBands.map((band) => (
-                        <Grid item columns={columns} xs={1} key={`total_${band}`}>
+                    {secondStatValues.map((v) => (
+                        <Grid item columns={columns} xs={1} key={`total_${v}`}>
                             <Typography>
                                 {Object.values(groups)
-                                    .map((group) => (group[band] || []).length)
+                                    .map((group) => (group[v] || []).length)
                                     .reduce((acc, curr) => acc + curr, 0)}
                             </Typography>
                         </Grid>
