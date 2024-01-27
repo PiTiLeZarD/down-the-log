fb899431329aa4d125502a73863ed207896f1b43
Jonathan Adami <pitilezard@gmail.com>
2024-01-27 23:05:13 +1000

    make typescript happy

--- src/pages/qso-form/wwff-info.tsx
+++ src/pages/qso-form/wwff-info.tsx
@@ -3,7 +3,7 @@ import wwff from "../../data/wwff.json";
 import { Typography } from "../../utils/theme/components/typography";
 import { GmapsChip } from "./gmaps-chip";
 
-export type WwffDatum = { name: string; locator: string; website: string; dxcc: string };
+export type WwffDatum = { name: string; locator: string };
 
 export type WwffInfoProps = {
     parkRef?: string;
@@ -15,6 +15,6 @@ export const WwffInfo: WwffInfoComponent = ({ parkRef }): JSX.Element => {
     if (!parkRef) return <></>;
     if (!(parkRef in wwff)) return <Typography>Unknown parkRef {parkRef}</Typography>;
 
-    const { name, locator } = (wwff as Record<string, WwffDatum>)[parkRef];
+    const { name, locator } = wwff[parkRef as keyof typeof wwff] as WwffDatum;
     return <GmapsChip text={name} locator={locator} zoom={14} />;
 };
