6c7243c5a4700544a3204df9474b8fec01123d12
Jonathan Adami <pitilezard@gmail.com>
2024-02-01 12:17:04 +1000

    handle the absence of locator a bit better

--- src/pages/qso-form/gmaps-chip.tsx
+++ src/pages/qso-form/gmaps-chip.tsx
@@ -11,9 +11,22 @@ export type GmapsChipProps = {
 export type GmapsChipComponent = React.FC<GmapsChipProps>;
 
 export const GmapsChip: GmapsChipComponent = ({ text = "gmaps", locator, zoom = 4 }): JSX.Element => {
+    const [buttonText, setButtonText] = React.useState<string>(text);
+
     const latlng = locator ? maidenhead2Latlong(locator) : undefined;
-    const url = latlng
-        ? `https://www.google.com/maps/place/${latlng.latitude}+${latlng.longitude}/@${latlng.latitude},${latlng.longitude},${zoom}z?entry=ttu`
-        : undefined;
-    return <Button variant="chip" text={text} colour={latlng ? "primary" : "grey"} url={url} />;
+    const url =
+        latlng &&
+        `https://www.google.com/maps/place/${latlng.latitude}+${latlng.longitude}/@${latlng.latitude},${latlng.longitude},${zoom}z?entry=ttu`;
+    const onPress = () => {
+        setButtonText("Missing Location!");
+        setTimeout(() => setButtonText(text), 1500);
+    };
+    return (
+        <Button
+            variant="chip"
+            text={buttonText}
+            colour={latlng ? "primary" : "grey"}
+            {...(locator ? { url } : { onPress })}
+        />
+    );
 };
