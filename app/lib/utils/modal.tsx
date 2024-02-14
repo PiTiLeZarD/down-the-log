bcf795cc1047dc1526963ffd55dc6d691d576614
Jonathan Adami <pitilezard@gmail.com>
2024-02-14 14:08:43 +1000

    fix the dark mode for the modal

--- app/lib/utils/modal.tsx
+++ app/lib/utils/modal.tsx
@@ -1,6 +1,14 @@
 import React from "react";
-import { Modal as RNModal } from "react-native";
+import { Modal as RNModal, View } from "react-native";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
 import { Grid } from "../components/grid";
+
+const stylesheet = createStyleSheet((theme) => ({
+    container: {
+        backgroundColor: theme.background,
+        flex: 1,
+    },
+}));
 
 export type ModalProps = {
     open: boolean;
@@ -11,24 +19,27 @@ export type ModalProps = {
 export type ModalComponent = React.FC<React.PropsWithChildren<ModalProps>>;
 
 export const Modal: ModalComponent = ({ open, wide, onClose, children }): JSX.Element => {
+    const { styles } = useStyles(stylesheet);
     return (
-        <RNModal animationType="none" visible={open} onRequestClose={onClose}>
-            <Grid container>
-                <Grid
-                    item
-                    {...(wide ? { xs: -1, md: 1, lg: 2, xl: 3, xxl: 4 } : { xs: 1, md: 2, lg: 3, xl: 4, xxl: 5 })}
-                />
-                <Grid
-                    item
-                    {...(wide ? { xs: 12, md: 10, lg: 8, xl: 6, xxl: 4 } : { xs: 10, md: 8, lg: 6, xl: 4, xxl: 2 })}
-                >
-                    {children}
+        <RNModal animationType="none" visible={open} onRequestClose={onClose} transparent>
+            <View style={styles.container}>
+                <Grid container>
+                    <Grid
+                        item
+                        {...(wide ? { xs: -1, md: 1, lg: 2, xl: 3, xxl: 4 } : { xs: 1, md: 2, lg: 3, xl: 4, xxl: 5 })}
+                    />
+                    <Grid
+                        item
+                        {...(wide ? { xs: 12, md: 10, lg: 8, xl: 6, xxl: 4 } : { xs: 10, md: 8, lg: 6, xl: 4, xxl: 2 })}
+                    >
+                        {children}
+                    </Grid>
+                    <Grid
+                        item
+                        {...(wide ? { xs: -1, md: 1, lg: 2, xl: 3, xxl: 4 } : { xs: 1, md: 2, lg: 3, xl: 4, xxl: 5 })}
+                    />
                 </Grid>
-                <Grid
-                    item
-                    {...(wide ? { xs: -1, md: 1, lg: 2, xl: 3, xxl: 4 } : { xs: 1, md: 2, lg: 3, xl: 4, xxl: 5 })}
-                />
-            </Grid>
+            </View>
         </RNModal>
     );
 };
