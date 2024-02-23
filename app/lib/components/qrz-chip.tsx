1878f5f1af378d234bd1e87f3139db051ee9ecb8
Jonathan Adami <pitilezard@gmail.com>
2024-02-23 20:56:56 +1000

    Extend the QRZ chip

--- app/lib/components/qrz-chip.tsx
+++ app/lib/components/qrz-chip.tsx
@@ -1,18 +1,26 @@
 import React from "react";
-import { parseCallsign } from "../utils/callsign";
+import { findCountry, getCallsignData, parseCallsign } from "../utils/callsign";
 import { Button } from "../utils/theme/components/button";
 
 export type QrzChipProps = {
     callsign: string;
+    textIsCallsign?: boolean;
+    includeFlag?: boolean;
 };
 
 export type QrzChipComponent = React.FC<QrzChipProps>;
 
-export const QrzChip: QrzChipComponent = ({ callsign }): JSX.Element => {
+export const QrzChip: QrzChipComponent = ({ callsign, textIsCallsign = false, includeFlag = false }): JSX.Element => {
     const parsed = parseCallsign(callsign);
     return (
         <Button
-            text="QRZ"
+            text={
+                textIsCallsign
+                    ? includeFlag
+                        ? `${findCountry(getCallsignData(callsign))?.flag} ${callsign}`
+                        : callsign
+                    : "QRZ"
+            }
             variant="chip"
             url={`https://www.qrz.com/db/${parsed?.prefix}${parsed?.index}${parsed?.delineation}`}
         />
