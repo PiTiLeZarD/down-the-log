f12efcb1a9e1f157bd730c0c269ea56d519d41cb
Jonathan Adami <pitilezard@gmail.com>
2024-03-28 09:12:06 +1000

    menu buttons gettings some love too

--- app/menu.tsx
+++ app/menu.tsx
@@ -1,8 +1,45 @@
 import { useRouter } from "expo-router";
 import React from "react";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
+import { Grid } from "./lib/components/grid";
 import { PageLayout } from "./lib/components/page-layout";
 import { Stack } from "./lib/components/stack";
 import { Button } from "./lib/utils/theme/components/button";
+import { Icon, IconName } from "./lib/utils/theme/components/icon";
+import { Typography } from "./lib/utils/theme/components/typography";
+
+const stylesheet = createStyleSheet((theme) => ({
+    menuButton: {
+        backgroundColor: theme.colours.primary[theme.shades.lighter],
+    },
+    menuButtonStack: {
+        height: 100,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+}));
+
+export type MenuButtonProps = {
+    navigateTo: string;
+    text: string;
+    icon: IconName;
+};
+
+export type MenuButtonComponent = React.FC<MenuButtonProps>;
+
+export const MenuButton: MenuButtonComponent = ({ navigateTo, icon, text }): JSX.Element => {
+    const { navigate } = useRouter();
+    const { styles } = useStyles(stylesheet);
+
+    return (
+        <Button onPress={() => navigate(navigateTo)} style={styles.menuButton}>
+            <Stack style={styles.menuButtonStack}>
+                <Icon name={icon} size={30} />
+                <Typography variant="h4">{text}</Typography>
+            </Stack>
+        </Button>
+    );
+};
 
 export type MenuProps = {};
 
@@ -12,15 +49,30 @@ const Menu: MenuComponent = (): JSX.Element => {
     const { navigate } = useRouter();
     return (
         <PageLayout title="Menu">
-            <Stack gap="xxl">
-                <Button startIcon="home" text="Home" onPress={() => navigate("/")} />
-                <Button startIcon="earth" text="Events" onPress={() => navigate("/events")} />
-                <Button startIcon="albums" text="QSLs" onPress={() => navigate("/qsl")} />
-                <Button startIcon="stats-chart" text="Stats" onPress={() => navigate("/stats")} />
-                <Button startIcon="settings" text="Settings" onPress={() => navigate("/settings")} />
-                <Button startIcon="archive" text="Import/Export" onPress={() => navigate("/adif")} />
-                <Button startIcon="information-circle" text="About" onPress={() => navigate("/about")} />
-            </Stack>
+            <Grid container>
+                <Grid item xs={6}>
+                    <MenuButton icon="earth" navigateTo="/events" text="Events" />
+                </Grid>
+                <Grid item xs={6}>
+                    <MenuButton icon="albums" navigateTo="/qsl" text="QSLs" />
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item xs={6}>
+                    <MenuButton icon="stats-chart" navigateTo="/stats" text="Stats" />
+                </Grid>
+                <Grid item xs={6}>
+                    <MenuButton icon="archive" navigateTo="/adif" text="Import/Export" />
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item xs={6}>
+                    <MenuButton icon="settings" navigateTo="/settings" text="Settings" />
+                </Grid>
+                <Grid item xs={6}>
+                    <MenuButton icon="information-circle" navigateTo="/about" text="About" />
+                </Grid>
+            </Grid>
         </PageLayout>
     );
 };
