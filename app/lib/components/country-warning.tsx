6c7cf230e4ae5021c88364bd4a2a93918e04ef66
Jonathan Adami <pitilezard@gmail.com>
2024-04-05 20:07:29 +1000

    change the country it'll set dxcc, continent, locator accordingly, also changing the callsign will update the country

--- app/lib/components/country-warning.tsx
+++ app/lib/components/country-warning.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useFormContext } from "react-hook-form";
+import { callsigns } from "../data/callsigns";
 import { collapseCallsign, getCallsignData } from "../utils/callsign";
 import { Alert } from "../utils/theme/components/alert";
 import { Typography } from "../utils/theme/components/typography";
@@ -10,15 +11,38 @@ export type CountryWarningProps = {};
 export type CountryWarningComponent = React.FC<CountryWarningProps>;
 
 export const CountryWarning: CountryWarningComponent = (): JSX.Element => {
-    const { watch } = useFormContext<QSO>();
+    const { watch, setValue, getValues } = useFormContext<QSO>();
     const callsign = watch("callsign");
     const country = watch("country");
-
     const csdata = getCallsignData(collapseCallsign(callsign));
-    if (!csdata || csdata.iso3 == country) return <></>;
-    return (
-        <Alert severity="warning">
-            <Typography>The country selected doesn't match the callsign</Typography>
-        </Alert>
-    );
+
+    useEffect(() => {
+        const cs = callsigns.find((c) => c.iso3 === country);
+        if (cs) {
+            if (cs.ctn != getValues("continent")) setValue("continent", cs.ctn);
+            if (+cs.dxcc != getValues("dxcc")) setValue("dxcc", +cs.dxcc);
+            if (cs.gs != getValues("locator")) setValue("locator", cs.gs);
+        }
+    }, [country]);
+
+    useEffect(() => {
+        if (csdata?.iso3 && csdata.iso3 != country) setValue("country", csdata.iso3);
+    }, [callsign]);
+
+    if (!csdata) return <></>;
+
+    if (csdata.iso3 !== country)
+        return (
+            <Alert severity="warning">
+                <Typography>The country selected doesn't match the callsign</Typography>
+            </Alert>
+        );
+    if (+csdata.dxcc !== getValues("dxcc"))
+        return (
+            <Alert severity="warning">
+                <Typography>The country selected doesn't match the dxcc</Typography>
+            </Alert>
+        );
+
+    return <></>;
 };
