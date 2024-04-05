644f5fe96d3e2763a3b99b0abcdd242baed48afa
Jonathan Adami <pitilezard@gmail.com>
2024-04-05 20:06:26 +1000

    change the locator, it'll set itu/cq zones accordingly

--- app/lib/components/form/locator-field.tsx
+++ app/lib/components/form/locator-field.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { useFormContext } from "react-hook-form";
-import { maidenDistance, normalise } from "../../utils/locator";
+import cqzones from "../../data/cqzones.json";
+import ituzones from "../../data/ituzones.json";
+import { maidenDistance, maidenhead2Latlong, normalise } from "../../utils/locator";
+import { findZone } from "../../utils/polydec";
 import { Typography } from "../../utils/theme/components/typography";
 import { GmapsChip } from "../gmaps-chip";
 import { QSO } from "../qso";
@@ -16,11 +19,23 @@ export type LocatorFieldComponent = React.FC<LocatorFieldProps>;
 
 export const LocatorField: LocatorFieldComponent = ({ name, label }): JSX.Element => {
     const { watch, getValues, setValue } = useFormContext<QSO>();
+
     useEffect(() => {
         const values = getValues();
         setValue(name, normalise(values[name]));
         if (values.myLocator && values.locator) setValue("distance", maidenDistance(values.myLocator, values.locator));
     }, [watch("locator"), watch("myLocator")]);
+
+    useEffect(() => {
+        const gs = getValues("locator");
+        if (gs) {
+            const cqzone = findZone(cqzones, maidenhead2Latlong(gs));
+            const ituzone = findZone(ituzones, maidenhead2Latlong(gs));
+            if (+cqzone != getValues("cqzone")) setValue("cqzone", +cqzone);
+            if (+ituzone != getValues("ituzone")) setValue("ituzone", +ituzone);
+        }
+    }, [watch("locator")]);
+
     return (
         <FormField
             name={name}
