2ec3cd41e73b9b7e4ed0f7dca3d0910b20651b5f
Jonathan Adami <pitilezard@gmail.com>
2024-04-11 09:35:27 +1000

    icons aplenty, have to redesign this part

--- app/lib/components/qso/qso-list-item.tsx
+++ app/lib/components/qso/qso-list-item.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { QSO, duration, hasEvent } from ".";
 import { countries } from "../../data/countries";
 import { roundTo } from "../../utils/math";
@@ -19,39 +19,45 @@ export type QsoListItemProps = {
 export type QsoListItemComponent = React.FC<QsoListItemProps>;
 
 export const QsoListItem: QsoListItemComponent = React.memo(
-    ({ item: qso, index, lineHeight, onQsoPress, imperial }): JSX.Element => (
-        <QsoRow
-            lineHeight={lineHeight}
-            success={qso.lotw_received || qso.eqsl_received}
-            position={String((qso.position === undefined ? index : qso.position) + 1)}
-            time={qso.date.toFormat("HH:mm")}
-            duration={duration(qso)}
-            callsign={
-                <Stack direction="row">
-                    <Typography>{qso.country ? countries[qso.country]?.flag : ""}</Typography>
-                    <Typography>{qso.callsign}</Typography>
-                    {qso.distance !== undefined && (
-                        <Typography>
-                            ({imperial ? roundTo(qso.distance / 1.6, 2) : qso.distance}
-                            {imperial ? "mi" : "km"})
-                        </Typography>
-                    )}
-                </Stack>
-            }
-            name={qso.name || "N/A"}
-            band={
-                hasEvent(qso) ? (
+    ({ item: qso, index, lineHeight, onQsoPress, imperial }): JSX.Element => {
+        const icons = [
+            qso.note ? <Icon name="chatbox-ellipses-outline" /> : null,
+            hasEvent(qso) ? <Icon name="earth" /> : null,
+            (qso.pota && qso.myPota) || (qso.wwff && qso.myWwff) ? <Icon name="swap-horizontal" /> : null,
+        ].filter((e) => !!e);
+
+        return (
+            <QsoRow
+                lineHeight={lineHeight}
+                success={qso.lotw_received || qso.eqsl_received}
+                position={String((qso.position === undefined ? index : qso.position) + 1)}
+                time={qso.date.toFormat("HH:mm")}
+                duration={duration(qso)}
+                callsign={
+                    <Stack direction="row">
+                        <Typography>{qso.country ? countries[qso.country]?.flag : ""}</Typography>
+                        <Typography>{qso.callsign}</Typography>
+                        {qso.distance !== undefined && (
+                            <Typography>
+                                ({imperial ? roundTo(qso.distance / 1.6, 2) : qso.distance}
+                                {imperial ? "mi" : "km"})
+                            </Typography>
+                        )}
+                    </Stack>
+                }
+                name={qso.name || "N/A"}
+                band={
                     <Stack direction="row">
                         <Typography>{[qso.band, qso.mode].filter((e) => !!e).join("/")}</Typography>
-                        <Icon name="earth" />
+                        {icons.map((icon, i) => (
+                            <Fragment key={i}>{icon}</Fragment>
+                        ))}
                     </Stack>
-                ) : (
-                    [qso.band, qso.mode].filter((e) => !!e).join("/")
-                )
-            }
-            onPress={() => onQsoPress(qso)}
-        />
-    ),
+                }
+                onPress={() => onQsoPress(qso)}
+            />
+        );
+    },
     (prevProps, nextProps) =>
         nextProps.item.id === prevProps.item.id && nextProps.item.position === prevProps.item.position,
 );
